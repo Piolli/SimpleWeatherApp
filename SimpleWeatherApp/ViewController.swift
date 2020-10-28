9d2b18882176e4bb759257f3b15d7b36d6d6cf12
Alexandr <pocharua@mail.ru>
2020-10-28 13:43:44 +0700

    Initial Commit

---
+++ SimpleWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by Александр Камышев on 28.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleWeatherAppTests/SimpleWeatherAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  SimpleWeatherAppTests.swift
+//  SimpleWeatherAppTests
+//
+//  Created by Александр Камышев on 28.10.2020.
+//
+
+import XCTest
+@testable import SimpleWeatherApp
+
+class SimpleWeatherAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
