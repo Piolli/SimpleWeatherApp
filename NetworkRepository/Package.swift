454db6556a2f9c3567dcabc4ed0605b224ee5eb0
Alexandr <pocharua@mail.ru>
2020-10-31 12:18:05 +0700

    Add network library to NetworkRepository module, also fix repository provider file name

--- NetworkRepository/Package.swift
+++ NetworkRepository/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkRepository",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,14 +15,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "Domain")
+        .package(path: "Domain"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NetworkRepository",
-            dependencies: ["Domain"]),
+            dependencies: ["Domain", "Alamofire"]),
         .testTarget(
             name: "NetworkRepositoryTests",
             dependencies: ["NetworkRepository", "Domain"]),
