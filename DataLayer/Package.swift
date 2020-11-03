384b2036bcbe5c061e7cdd041b5eaa87e30d0ab9
Alexandr <pocharua@mail.ru>
2020-11-03 22:41:37 +0700

    Add RealmSwift dependency to DataLayer module

--- DataLayer/Package.swift
+++ DataLayer/Package.swift
@@ -14,16 +14,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "Domain")
+        .package(path: "Domain"),
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", .upToNextMajor(from: "10.1.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on
+        
         .target(
             name: "DataLayer",
-            dependencies: ["Domain"]),
+            dependencies: ["Domain", .product(name: "RealmSwift", package: "Realm")]),
         .testTarget(
             name: "DataLayerTests",
-            dependencies: ["DataLayer", "Domain"]),
+            dependencies: ["DataLayer", "Domain", ]),
     ]
 )
